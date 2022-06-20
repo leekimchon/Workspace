ac7b7ec60cecf684595735ca9283f55f74468320
Chơn Lê <leekimchon01@gmail.com>
2022-06-20 14:08:56 +0700

    20/06/2022

--- BaiTapModule2/Shop_May_Tinh/mvc/layouts/layout_admin/header.php
+++ BaiTapModule2/Shop_May_Tinh/mvc/layouts/layout_admin/header.php
@@ -8,8 +8,9 @@
         <meta name="author" content="" />
         <title>Admin Page</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="/public/css/styles.css" rel="stylesheet" />
-        <link href="/public/css/customstyle.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/BaiTapModule2/Shop_May_Tinh/public/css/styles.css">
+        <!-- <link href="/public/css/styles.css" rel="stylesheet" /> -->
+        <link href="/BaiTapModule2/Shop_May_Tinh/public/css/customstyle.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     </head>
