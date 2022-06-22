66a192dd9ea5b5887b92415eea34ea162042a460
leekimchon <leekimchon@gmail.com>
2022-06-22 08:14:44 +0700

    22/06/2022

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // i++ tương đương với i = i + 1 hoặc i += 1.
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
